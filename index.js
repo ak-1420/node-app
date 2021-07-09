def375d68c8ff1e6f4d1339995dc6b7ad27f957f
arunkumarSouhel <60317193+ak-1420@users.noreply.github.com>
2021-07-09 21:45:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app  = express()
 const port = process.env.PORT || 1420
 
 app.get('/',(req,res)=>{
-    res.send('<center> <h1 style="color:blue"> Welcome to My Youtube Channel </h1> </center>')
+    res.send('<center> <h1 style="color:blue"> Welcome to My Youtube Channel Navvuthu Nerchuko</h1> </center>')
 })
 
 
